e1ddb24d9808a46058a08bf105b4938755816306
Hadi-AlKammouni <hadi-kam99@hotmail.com>
2022-05-21 23:55:30 +0300

    Calling the js code + Declaring several needed variables

--- game.js
+++ game.js
@@ -1,1 +1,31 @@
 /* Javascript Code of the Maze Game */
+
+// Calling the javascript code from the html file 
+window.onload = function () {
+    
+    // Declaring the variable status + changing the element's content & color
+    let status = document.getElementById("status")
+    status.innerHTML="Begin by clicking the letter S <br/> Your score is 0 / If you win you get 5 pts / If you lose you get -10 pts"
+    status.style.color="red"
+    
+    // Declaring the variable boundary
+    let boundary=document.getElementsByClassName("boundary")
+
+    // Declaring the variable win
+    let win = document.getElementById("end")
+
+    // Declaring the variable start
+    let start = document.getElementById("start")
+    
+    
+    // Declaring the variable score to track the user score during the game (win ==> +5 / lose ==> -10)
+    let score = 0
+
+    // Declaring the variable restart_game to accomplish all conditions upon restarting the game
+    let refresh_game = "off"  
+    
+    // Upon clicking on letter S, the game started by calling the startGame function
+    start.onclick = function() {startGame()}
+
+    
+}
